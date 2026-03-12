005d75211a1d387d8b54eddea4d3272c67116b98
Raguram927 <raguram9271@gmail.com>
2026-03-12 16:26:48 +0530

    Add files via upload

---
+++ lab expriments.py
@@ -0,0 +1,30 @@
+'''n=int(input("enter a number"))
+for n in range (1,101,1):
+      print(n)
+
+n=int(input("enter a number"))
+for i in range (1,50,3):
+    print(i)
+if n%3==0:
+    print(i)'''
+
+'''n=int(input("enter a number"))
+for i in range (1,11,1):
+    if i==9:
+      break
+print (i)
+n=int(input("enter a number"))
+count=0
+while n>0:
+    n=n//10
+    count+=1
+print ("count of digit is:", count)
+'''
+
+'''n=str(input("enter a number"))
+for num in n:
+    if num == 5:
+        pass  
+    else:
+        print(f"Processing {num}")
+'''
